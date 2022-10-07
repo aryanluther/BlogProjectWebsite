e7fa278dc26b85c511777c306096f4a2f890db6f
aryanluther <aryanluthra2014@gmail.com>
2022-10-07 18:39:40 -0400

    finished

---
+++ blog-website/components/navbar.js
@@ -0,0 +1,13 @@
+function navbar() {
+  return (
+    <>
+      <nav className="justify-center flex flex-wrap py-3 shadow-lg">
+        <div className="font-mono">
+          <h1 className="text-3xl text-center">Project Blogo</h1>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default navbar;

e77a8feffefa1fa20c48b6edde12def7bd9bc2e3
aryanluther <aryanluthra2014@gmail.com>
2022-10-08 13:04:54 -0400

    blog post file added

--- blog-website/pages/index.js
+++ blog-website/pages/index.js
@@ -1,11 +1,13 @@
 import NavBar from "./components/navbar";
 import Landing from "./components/landing";
+import Blogpost from "./components/blogpost";
 
 export default function Home() {
   return (
     <div className="bg-orange-50">
       <NavBar />
       <Landing />
+      <Blogpost />
     </div>
   );
 }
